94d83ecb1ffe38af0cf2d6aa43da01c0bd4f0ccd
Andrey Gorobets <avgorobets@avito.ru>
2022-05-14 23:54:52 +0300

    Создана базовая структура приложения

---
+++ src/main.py
@@ -0,0 +1,54 @@
+import logging
+
+import aioredis
+import uvicorn as uvicorn
+from elasticsearch import AsyncElasticsearch
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from api.v1 import films
+from core import config
+from core.logger import LOGGING
+from db import elastic, redis
+
+app = FastAPI(
+    title=config.PROJECT_NAME,
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
+    default_response_class=ORJSONResponse,
+)
+
+
+@app.on_event('startup')
+async def startup():
+    """
+    Подключаемся к базам при старте сервера
+    Подключиться можем при работающем event-loop
+    Поэтому логика подключения происходит в асинхронной функции
+    :return:
+    """
+    redis.redis = aioredis.from_url(f'redis://{config.REDIS_HOST}:{config.REDIS_PORT}')
+    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_SCHEME}://{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    """Отключаемся от баз при выключении сервера.
+
+    :return:
+    """
+    await redis.redis.close()
+    await elastic.es.close()
+
+
+app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host=config.APP_HOST,
+        port=config.APP_PORT,
+        reload=config.APP_LIVE_RELOAD,
+        log_config=LOGGING,
+        log_level=logging.DEBUG,
+    )
