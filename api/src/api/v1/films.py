d33fd70002c2682e65634e24b181e090df5a8836
Sergey <originalustinov@gmail.com>
2022-05-17 22:32:40 +0300

    Debug stub for films api
    _ #5

--- api/src/api/v1/films.py
+++ api/src/api/v1/films.py
@@ -1,19 +1,21 @@
+import logging
 from http import HTTPStatus
+from typing import Union
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request, Query
 from pydantic import BaseModel
 
 from services.film import FilmService, get_film_service
 
 router = APIRouter()
 
+logger = logging.getLogger(__name__)
+
 
 class Film(BaseModel):
-    id: UUID
+    id: str
     title: str
-    type: str
-    imdb_rating: float
 
 
 @router.get('/{film_id}', response_model=Film)
@@ -21,10 +23,48 @@ async def film_details(film_id: str, film_service: FilmService = Depends(get_fil
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+    return Film(id=film.id, title=film.title)
 
-    return Film(
-        id=film.id,
-        title=film.title,
-        type=film.type,
-        imdb_rating=film.imdb_rating
+
+@router.get("/")
+async def films_by_imdb(
+        sort: Union[str, None],
+        pg_size: Union[int, None] = Query(default=None, alias="page[size]"),
+        pg_number: Union[int, None] = Query(
+            default=None,
+            alias="page[number]"
+        ),
+        fltr: Union[str, None] = Query(default=None, alias="filter[genre]")
+) -> int:
+    if fltr:
+        await get_films(pg_size, pg_number, sort)
+    else:
+        await get_films(pg_size, pg_number, sort)
+    return 0
+
+
+async def get_films(chunk_size: int, chunk_number: int, sort_type: str):
+    logger.debug(
+        f"Films request: page: {chunk_number}, entries: {chunk_size},"
+        f" sorted by {sort_type}"
     )
+    return 0
+
+
+async def get_filtred_films(
+        fltr: str,
+        chunk_size: int,
+        chunk_number: int,
+        sort_type: str
+):
+    logger.debug(
+        f"Films request with filter: filter {fltr}, page: {chunk_number},"
+        f" entries: {chunk_size}, sorted by {sort_type}"
+    )
+    return 1
+
+
+@router.get("/search")
+async def search_films(request: Request) -> int:
+    print(request)
+    return 0
