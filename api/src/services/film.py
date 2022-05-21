8e6f850e38b62568ddb73e2d9c796f60a8144e9b
Sergey <originalustinov@gmail.com>
2022-05-21 15:52:53 +0300

    separate Redis, Elastic and main Film service
    _ #5

--- api/src/services/film.py
+++ api/src/services/film.py
@@ -1,20 +1,30 @@
+import logging
 from functools import lru_cache
-from typing import Optional, Union
+from typing import Optional
 
 from aioredis import Redis
-from elasticsearch import AsyncElasticsearch, NotFoundError
+from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
 
 from db.elastic import get_elastic
 from db.redis import get_redis
 from models.film import Film
 
-FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
-
-SearchParam = dict[str, Union[str, int]]
-
 DEFAULT_PAGE = 1
 DEFAULT_PAGE_SIZE = 50
+
+
+class Offset:
+    def __init__(self, offset: int, size: int):
+        self.offset = offset
+        self.size = size
+
+
+def convert(page_id: Optional[int], page_size: Optional[int]) -> Offset:
+    page = abs(page_id) if page_id else DEFAULT_PAGE
+    page_size = abs(page_size) if page_size else DEFAULT_PAGE_SIZE
+    offset = (page - 1 if page else 0) * page_size
+    return Offset(offset, page_size)
 
 
 class FilmService:
@@ -22,21 +32,22 @@ class FilmService:
         self.redis = redis
         self.elastic = elastic
         self.index = index
+        self._log = logging.getLogger("FilmService")
 
     async def get_by_id(self, film_id: str) -> Optional[Film]:
-        """ Возвращает объект фильма. Он опционален, так как фильм может отсутствовать в базе
+        """ Get Film by Id
 
         :param film_id:
         :return:
         :rtype: Optional[Film]
         """
-        film = await self._film_from_cache(film_id)
-        if not film:
-            film = await self._get_film_from_elastic(film_id)
-            if not film:
-                return None
-            await self._put_film_to_cache(film)
+        if film := await self._cache.get_one(film_id):
+            self._log.debug("Film from cache")
+            return film
 
+        film = await self._elastic.get_one(film_id)
+        if film:
+            await self._cache.put_one(film)
         return film
 
     async def search(
@@ -45,152 +56,62 @@ class FilmService:
             page: Optional[int],
             page_size: Optional[int]
     ) -> list[Film]:
-        page = page if page else DEFAULT_PAGE
-        page_size = page_size if page_size else DEFAULT_PAGE_SIZE
-        offset = page * page_size
-        films = await self._search_from_cache(query, offset, page_size)
-        if not films:
-            films = await self._get_search_from_elastic(
-                query,
-                offset,
-                page_size
-            )
-            if films:
-                params = {
-                    "query": query,
-                    "offset": offset,
-                }
-                await self._put_search_to_cache(params, films)
+        """ Search corresponding films
+
+        @param query:
+        @param page:
+        @param page_size:
+        @return: list[Film]
+        """
+        pg = convert(page, page_size)
+        if films := await self._cache.get_search(query, pg.offset, pg.size):
+            self._log.debug("Search results from cache")
+            return films
+        resp = await self._elastic.get_search(query, pg.offset, pg.size)
+        films = [Film(**entry['_source']) for entry in resp]
+        if films:
+            await self._cache.put_search(query, pg.offset, films)
         return films
 
     async def get_films(
             self,
             page: Optional[int],
             page_size: Optional[int],
-            fltr: Optional[str],
+            fltr_genre: Optional[str],
             sort_type: Optional[str]
     ) -> list[Film]:
-        page = page if page else DEFAULT_PAGE
-        page_size = page_size if page_size else DEFAULT_PAGE_SIZE
-        offset = page * page_size
+        """ Get films by parameters
 
-        return await self._get_films_from_elastic(
-            offset,
-            page_size,
-            fltr,
+        @param page:
+        @param page_size:
+        @param fltr_genre:
+        @param sort_type:
+        @return: list[Film]
+        """
+        pg = convert(page, page_size)
+        if films := await self._cache.get_films(
+                pg.offset,
+                pg.size,
+                fltr_genre,
+                sort_type
+        ):
+            self._log.debug("Films from cache")
+            return films
+        resp = await self._elastic.get_films(
+            pg.offset,
+            pg.size,
+            fltr_genre,
             sort_type
         )
-
-    async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
-        try:
-            doc = await self.elastic.get(index=self.index, id=film_id)
-        except NotFoundError:
-            return None
-        return Film(**doc['_source'])
-
-    async def _get_search_from_elastic(
-            self,
-            query: str,
-            offset: int = 0,
-            max_size: int = 50,
-            sort_by: Optional[str] = "-imdb_rating"
-    ) -> list[Film]:
-        # If we're going to show more than 10k, we should use 'search_after'
-        sort = [{sort_by: {"order": "asc"}, }, ] if sort_by else None
-        matching = {
-            "multi_match": {
-                "query": query,
-                "fields": ['title', 'description'],
-                "operator": "or",
-            }
-        }
-        try:
-            resp = await self.elastic.search(
-                index="movies",
-                body={
-                    "query": matching
-                },
-                from_=offset,
-                size=max_size,
-                sort=sort,
+        films = [Film(**entry['_source']) for entry in resp]
+        if films:
+            await self._cache.put_films(
+                pg.offset,
+                fltr_genre,
+                sort_type,
+                films
             )
-        except NotFoundError:
-            return []
-        return resp['hits']['hits']
-
-    async def _get_films_from_elastic(
-            self,
-            offset: Optional[int],
-            size: Optional[int],
-            fltr: Optional[str],
-            sort_by: Optional[str]
-    ) -> list[Film]:
-        # If we're going to show more than 10k, we should use 'search_after'
-        sort = [{sort_by: {"order": "asc"}, }, ] if sort_by else None
-        query = {
-            "bool": {
-                "must": {
-                    "match_all": {}
-                },
-                "filter": {
-                    "term": {fltr}
-                }
-            }
-        } if fltr else {'match_all': {}}
-        try:
-            resp = await self.elastic.search(
-                index="movies",
-                body={
-                    "query": query
-                },
-                from_=offset,
-                size=size,
-                sort=sort,
-            )
-        except NotFoundError:
-            return []
-        return resp['hits']['hits']
-
-    async def _film_from_cache(self, film_id: str) -> Optional[Film]:
-        """
-        Пытаемся получить данные о фильме из кеша, используя команду get
-        https://redis.io/commands/get
-
-        :param film_id:
-        :return:
-        :rtype: Optional[Film]
-        """
-        data = await self.redis.get(film_id)
-        if not data:
-            return None
-
-        # pydantic предоставляет удобное API для создания объекта моделей из json
-        film = Film.parse_raw(data)
-        return film
-
-    async def _search_from_cache(
-            self,
-            query: str,
-            offset: int = 0,
-            max_size: int = 50
-    ) -> list[Film]:
-        return []
-
-    async def _put_film_to_cache(self, film: Film):
-        # Сохраняем данные о фильме, используя команду set
-        # Выставляем время жизни кеша — 5 минут
-        # https://redis.io/commands/set
-        # pydantic позволяет сериализовать модель в json
-        await self.redis.set(film.id, film.json(),
-                             ex=FILM_CACHE_EXPIRE_IN_SECONDS)
-
-    async def _put_search_to_cache(
-            self,
-            params: SearchParam,
-            films: list[Film]
-    ) -> bool:
-        """ Put the search parameters and results in cahce"""
-        return False
+        return films
 
 
 @lru_cache()
---
+++ api/src/services/film_elastic.py
@@ -0,0 +1,122 @@
+from typing import Optional
+import logging
+
+from elasticsearch import AsyncElasticsearch, NotFoundError
+
+from models.film import Film
+
+ELASTIC_INDEX = "movies"
+ELASTIC_SEARCH_FIELDS = ['title', 'description']
+
+
+class FilmElasticService:
+    def __init__(self, elastic: AsyncElasticsearch):
+        self._elastic = elastic
+        self.logger = logging.getLogger("FilmElasticService")
+
+    async def get_one(self, film_id: str) -> Optional[Film]:
+        """
+
+        @param film_id:
+        @return:
+        """
+        try:
+            doc = await self._elastic.get(index=ELASTIC_INDEX, id=film_id)
+        except NotFoundError:
+            self.logger.exception("")
+            return None
+        film = doc.get('_source')
+        return Film(**film) if film else None
+
+    async def get_search(
+            self,
+            query: str,
+            offset: int = 0,
+            max_size: int = 50,
+            sort_by: Optional[str] = None
+    ) -> list[Film]:
+        """
+
+        @param query:
+        @param offset:
+        @param max_size:
+        @param sort_by:
+        @return:
+        """
+        # If we're going to show more than 10k, we should use 'search_after'
+        matching = {
+            "multi_match": {
+                "query": query,
+                "fields": ELASTIC_SEARCH_FIELDS,
+                "operator": "or",
+            }
+        }
+        try:
+            resp = await self._elastic.search(
+                index=ELASTIC_INDEX,
+                body={
+                    "query": matching
+                },
+                from_=offset,
+                size=max_size,
+                sort=self._get_sort_query(sort_by),
+            )
+        except NotFoundError:
+            self.logger.exception("")
+            return []
+        try:
+            return resp['hits']['hits']
+        except KeyError:
+            self.logger.exception("Answer doesn't contain films")
+            return []
+
+    async def get_films(
+            self,
+            offset: Optional[int],
+            size: Optional[int],
+            genre_id: Optional[str],
+            sort_by: Optional[str]
+    ) -> list[Film]:
+        """
+
+        @param offset:
+        @param size:
+        @param genre_id:
+        @param sort_by:
+        @return:
+        """
+        # If we're going to show more than 10k, we should use 'search_after'
+        query = {
+            "bool": {
+                "must": {
+                    "match": {"genre.id": genre_id}}
+            },
+        } if genre_id else {'match_all': {}}
+        try:
+            resp = await self._elastic.search(
+                index=ELASTIC_INDEX,
+                body={
+                    "query": query
+                },
+                from_=offset,
+                size=size,
+                sort=self._get_sort_query(sort_by),
+            )
+        except NotFoundError:
+            self.logger.exception("")
+            return []
+        try:
+            return resp['hits']['hits']
+        except KeyError:
+            self.logger.exception("Answer doesn't contain films")
+            return []
+
+    @staticmethod
+    def _get_sort_query(sort_by: Optional[str]) -> Optional[dict]:
+        sort = None
+        if sort_by:
+            if sort_by[0] == '-':
+                sort = [{sort_by[1:]: {"order": "desc"}, }, ]
+            else:
+                sort = [{sort_by: {"order": "asc"}, }, ]
+        return sort
