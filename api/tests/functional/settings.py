abb35409b017e7981f8b4fbbd320ca0acd7a5edc
Sergey <originalustinov@gmail.com>
2022-06-12 13:39:53 +0300

    Fix review remarks
    #41

--- api/tests/functional/settings.py
+++ api/tests/functional/settings.py
@@ -1,23 +1,21 @@
-import os
-
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    API_SCHEME: str = os.environ.get('API_SCHEME', 'http')
-    API_HOST: str = os.environ.get('API_HOST', '127.0.0.1')
-    API_PORT: int = os.environ.get('API_PORT', 8000)
+    API_SCHEME: str = 'http'
+    API_HOST: str = '127.0.0.1'
+    API_PORT: int = 8000
 
-    REDIS_HOST: str = os.environ.get('REDIS_HOST', '127.0.0.1')
-    REDIS_PORT: int = os.environ.get('REDIS_PORT', 6379)
+    REDIS_HOST: str = '127.0.0.1'
+    REDIS_PORT: int = 6379
 
-    ELASTIC_SCHEME: str = os.environ.get('ELASTIC_SCHEME', 'http')
-    ELASTIC_HOST: str = os.environ.get('ELASTIC_HOST', '127.0.0.1')
-    ELASTIC_PORT: int = os.environ.get('ELASTIC_PORT', 9200)
+    ELASTIC_SCHEME: str = 'http'
+    ELASTIC_HOST: str = '127.0.0.1'
+    ELASTIC_PORT: int = 9200
 
-    SECRET_KEY: str = os.environ.get('SECRET_KEY', '')
+    SECRET_KEY: str = ''
 
-    LOGGING_LEVEL: str = os.environ.get('LOGGING_LEVEL', 'INFO')
+    LOGGING_LEVEL: str = 'INFO'
 
 
 settings = Settings()
